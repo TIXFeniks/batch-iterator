6a1df5f5c053c0243df00ba2b63a4e87cf67f5a7
TIXFeniks <dimdi-y@ya.ru>
2017-04-13 21:41:55 +0300

    Update batch_iterator.py

--- batch_iterator.py
+++ batch_iterator.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 def iterate_minibatches(*to_split, batchsize=1, shuffle=False):
+    """
+        generates batches from the data, passed as first arguments
+        batchsize: the number of rows in a batch
+        shuffle: if True shuffles the data and yields shuffled batches
+    """
     res = [np.array(x) for x in to_split]
+    size = res[0].shape[0]
     for x in res:
-        assert x.shape[0] == res[0].shape[0]
+        assert x.shape[0] == size
     if shuffle:
-        indices = np.arange(len(inputs))
+        indices = np.arange(size)
         np.random.shuffle(indices)
-    for start_idx in range(0, len(inputs), batchsize):
+    for start_idx in range(0, size, batchsize):
         if shuffle:
             excerpt = indices[start_idx:start_idx + batchsize]
         else:
